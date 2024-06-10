efd6ab69a77986ecc087ea6fe5f9a582782f8178
agolubov <agolubov@moysklad.ru>
2024-06-11 00:37:39 +0300

    Homework 2 - Правки ревью - оптимизации рендеринга таблицы

--- src/app/contragents/table/view.js
+++ src/app/contragents/table/view.js
@@ -3,6 +3,7 @@ import html from './index.html'
 export default class View {
     #table
     #template
+    #rows = new Map()
 
     #onDeleteAction
     #onEditAction
@@ -14,30 +15,48 @@ export default class View {
         this.#template = targetElement.querySelector('template')
     }
 
+    // данный метод позволяет отобразить любой новый список, сохраняя эффективное переиспользование dom
+    // в простых случаях подразумеваемых задачей (вставка в конец, удаление в любом месте, изменение одного элемента) -
+    // будет изменена только одна строка в таблице
     refresh(contragents) {
-        // remove old
-        this.#table.querySelectorAll('tr').forEach(el => this.#table.removeChild(el))
+        this.#removeRows(contragents)
+        const newRows = new Map()
 
-        // fill new
-        contragents.forEach(c => this.#table.appendChild(this.#createRow(c)))
+        contragents.forEach((contragent, idx) => {
+            let found = this.#rows.get(contragent.id)
+
+            if (found !== undefined) {
+                // На случай, если изменился порядок элементов
+                if (Array.prototype.indexOf.call(this.#table.children, found.asElement()) !== idx) {
+                    found.remove()
+                    this.#insertRowAt(found.asElement(), idx)
+                }
+            } else {
+                // Новый элемент может быть вставлен в любое место
+                found = new ContragentRow(this.#template, this.#onEditAction, this.#onDeleteAction)
+                this.#insertRowAt(found.asElement(), idx)
+            }
+
+            found.render(contragent)
+            newRows.set(contragent.id, found)
+        })
+
+        this.#rows = newRows
     }
 
-    #createRow(contragent) {
-        const row = this.#template.content.cloneNode(true)
+    #removeRows(contragents) {
+        const newIds = new Set(contragents.map(c => c.id))
+        Array.from(this.#rows.keys())
+            .filter(id => !newIds.has(id))
+            .forEach(id => this.#rows.get(id).remove())
+    }
 
-        const root = row.querySelector('tr')
-        root.addEventListener('dblclick', () => this.#onEditAction(contragent))
-
-        const elements = root.children
-        elements[0].textContent = contragent.name
-        elements[1].textContent = contragent.address
-        elements[2].textContent = contragent.inn
-        elements[3].textContent = contragent.kpp
-
-        const deleteButton = row.querySelector('button')
-        deleteButton.addEventListener('click', () => this.#onDeleteAction(contragent.id))
-
-        return row
+    #insertRowAt(child, index) {
+        if (index >= this.#table.children.length) {
+            this.#table.appendChild(child);
+        } else {
+            this.#table.insertBefore(child, this.#table.children[index]);
+        }
     }
 
     bindDelete(onDeleteAction) {
@@ -46,5 +65,58 @@ export default class View {
 
     bindEdit(onEditAction) {
         this.#onEditAction = onEditAction
+    }
+}
+
+class ContragentRow {
+    #root
+    #onEdit
+    #onDelete
+    #contragent
+
+    constructor(template, onEdit, onDelete) {
+        this.#onEdit = onEdit
+        this.#onDelete = onDelete
+
+        const row = template.content.cloneNode(true)
+
+        this.#root = row.querySelector('tr')
+        this.#root.addEventListener('dblclick', () => this.#onEditAction())
+
+        const deleteButton = row.querySelector('button')
+        deleteButton.addEventListener('click', () => this.#onDeleteAction())
+    }
+
+    render(contragent) {
+        if (this.#contragent === contragent) {
+            return
+        }
+        this.#contragent = contragent
+
+        const elements = this.#root.children
+        elements[0].textContent = contragent.name
+        elements[1].textContent = contragent.address
+        elements[2].textContent = contragent.inn
+        elements[3].textContent = contragent.kpp
+    }
+
+    remove() {
+        this.#root.remove()
+    }
+
+    asElement() {
+        return this.#root
+    }
+
+    #onEditAction() {
+        if (this.#contragent !== null && this.#contragent !== undefined) {
+            this.#onEdit(this.#contragent)
+        }
+    }
+
+    #onDeleteAction() {
+        if (this.#contragent !== null && this.#contragent !== undefined) {
+            this.#onDelete(this.#contragent.id)
+        }
     }
 }
